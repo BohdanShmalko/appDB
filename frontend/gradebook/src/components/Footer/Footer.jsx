b9a1f4da9a8255950cb8189c4311670e6be31936
Bohdan <bshmalko97@gmail.com>
2020-11-04 00:09:19 +0200

    add simple header and footer

--- frontend/gradebook/src/components/Footer/Footer.jsx
+++ frontend/gradebook/src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import s from './Footer.module.css'
+import logo from '../../img/icon.png'
 
 const Footer = (props) =>{
     return <footer className={s.footer}>
-        footer
+        <div className={s.logo}> <img src={logo} /> </div>
+        <div className={s.content}>Даний проект - курсова робота з дисципліни Бази данних-2. Створений студентом на 3 курсі Шмальком Богданом</div>
     </footer>
 }
 
--- frontend/gradebook/src/components/Header/Header.jsx
+++ frontend/gradebook/src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import s from './Header.module.css'
+import logo from '../../img/icon.png'
 
 const Header = (props) =>{
-    return <header className={s.gradebook}>
-        header
+    const isAuthorize = false
+    return <header className={s.header}>
+        <div className={s.logo}> <img src={logo} /> </div>
+        <div className={s.buttons}>
+            <button>Розклад</button>
+            {isAuthorize
+                ?<button>Вийти</button>
+                :<button>Ввійти</button>
+            }
+        </div>
     </header>
 }
 
